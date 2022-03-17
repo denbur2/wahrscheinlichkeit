c7e0f9395140ebd5172d093f1425d88837b3cf93
Hendrik Armbrecht <63508491+denbur2@users.noreply.github.com>
2022-03-17 13:23:40 +0100

    jaaa

--- code.py
+++ code.py
@@ -2,10 +2,13 @@ import random
 import time
 import os
 
+from numpy import sort
+
 
 würfelcounter = 0
 ratecounter = 0
-register = [[0],[0],[0],[0],[0],[0]]
+ratecounter2 = 0
+register = [[1, 0],[2, 0],[3, 0],[4, 0],[5, 0],[6, 0]]
 
 while True:
     würfelcounter += 1
@@ -13,7 +16,7 @@ while True:
     gewürfelt = random.randint(1, 6)
     for i in range(7):
         if gewürfelt == i:
-            register[i-1][0] = register[i-1][0]+1
+            register[i-1][1] = register[i-1][1]+1
 
 
 
@@ -21,13 +24,19 @@ while True:
     geraten = random.randint(1, 6)
     if geraten == gewürfelt:
         ratecounter += 1
-    
+
+    #raten2
+    kurzregister = (sorted(register, key=lambda x:x[1]))
+    if kurzregister[0][1] == gewürfelt:
+        ratecounter2 += 1
     #mit wahrscheinlichkeit raten
 
 
     
 
-    print("richtig geraten:", ratecounter,"\nwürfelcounter:", würfelcounter)
+    print("richtig geraten:", ratecounter, round((ratecounter/würfelcounter)*100, 3), "%")
+    print("richtig berechnet:", ratecounter2, round((ratecounter2/würfelcounter)*100, 3), "%")
+    print("würfelcounter:", würfelcounter)
     print("register: ",register)
 
 
